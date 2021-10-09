68be8c1b91b1e253fa002f824dac8346b5b55fa3
Jonas Heineson <heineson@gmail.com>
2021-10-09 09:48:11 +0200

    Frontend: configure Prettier

--- webapp/.eslintrc.js
+++ webapp/.eslintrc.js
@@ -1,23 +1,19 @@
 // eslint-disable-next-line no-undef
 module.exports = {
   root: true,
-  parser: "@typescript-eslint/parser",
-  plugins: [
-    "@typescript-eslint",
-    "eslint-plugin-react",
-    "eslint-plugin-react-hooks",
-    "prettier",
-  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'eslint-plugin-react', 'eslint-plugin-react-hooks', 'prettier'],
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended",
-    "plugin:react-hooks/recommended",
-    "plugin:testing-library/react",
-    "plugin:jest-dom/recommended",
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:testing-library/react',
+    'plugin:jest-dom/recommended',
+    'plugin:prettier/recommended',
+    'prettier',
   ],
   rules: {
-    "@typescript-eslint/explicit-module-boundary-types": "off",
-    "@typescript-eslint/no-explicit-any": "off",
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
   },
 };
