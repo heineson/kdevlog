179f7e94a160b47b3b491cf6fda7b6f665c0968f
Jonas Heineson <heineson@gmail.com>
2021-11-11 23:19:50 +0100

    Show notifications added to store

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,8 +1,25 @@
 import React from 'react';
 import './App.css';
 import MainMenu from './components/MainMenu';
+import NotificationContainer from './components/NotificationContainer';
+import { useNotificationStore } from './store/notifications';
+import { Button } from '@chakra-ui/react';
 
 function App() {
+  const [notifications, addNotification, removeNotification] = useNotificationStore((state) => [
+    state.notifications,
+    state.addNotification,
+    state.dismissNotification,
+  ]);
+
+  const handleAddNotification = () => {
+    addNotification({
+      title: 'test',
+      type: 'error',
+      message: 'desc',
+    });
+  };
+
   return (
     <MainMenu>
       <div className="App">
@@ -13,8 +30,12 @@ function App() {
           <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
             Learn React
           </a>
+          <Button colorScheme={'blue'} onClick={handleAddNotification}>
+            Test add notification
+          </Button>
         </header>
       </div>
+      <NotificationContainer notifications={notifications} removeNotification={removeNotification} />
     </MainMenu>
   );
 }
---
+++ webapp/src/components/NotificationContainer/index.tsx
@@ -0,0 +1,31 @@
+import { useEffect } from 'react';
+import { useToast } from '@chakra-ui/react';
+import { Notification } from '../../store/notifications';
+
+type NotificationContainerProps = {
+  notifications: Notification[];
+  removeNotification: (id: string) => void;
+};
+
+const NotificationContainer = ({ notifications, removeNotification }: NotificationContainerProps) => {
+  const toast = useToast();
+
+  useEffect(() => {
+    if (notifications && notifications.length > 0) {
+      const n = notifications.pop();
+      if (n) {
+        toast({
+          title: n.message,
+          status: n.type,
+          duration: 9000,
+          isClosable: true,
+        });
+        removeNotification(n.id);
+      }
+    }
+  }, [notifications, removeNotification, toast]);
+
+  return <div />;
+};
+
+export default NotificationContainer;
